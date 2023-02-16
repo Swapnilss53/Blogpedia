f77ad6cbb4878a28a9949ff66c29da62079f7fc6
Swapnil suresh shinde <91245012+Swapnilss53@users.noreply.github.com>
2023-02-16 12:06:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Server started on port ",port);
 });
